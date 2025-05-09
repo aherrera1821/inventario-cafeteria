030b790c521bfa05cf0364b3a2f9403faafce413
Alvaro <aherreraro18@gmail.com>
2025-05-08 23:36:36 -0600

    Actualiza db.py para conexión segura con st.secrets

--- db.py
+++ db.py
@@ -1,14 +1,12 @@
 import psycopg2
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
+import streamlit as st
 
 def get_connection():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        port=os.getenv("DB_PORT")
+        host=st.secrets["DB_HOST"],
+        dbname=st.secrets["DB_NAME"],
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASS"],
+        port=st.secrets["DB_PORT"]
     )
+
